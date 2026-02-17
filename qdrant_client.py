c24838fcf7157c1d155fc704bd08848337d2bb70
= <dbrizuela@zoonsuite.com>
2026-02-17 00:27:16 -0300

    update

--- qdrant_client.py
+++ qdrant_client.py
@@ -13,9 +13,20 @@ Notas:
 """
 
 import os
+import logging
 from dotenv import load_dotenv
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s -\n %(message)s'
+)
+
+logger = logging.getLogger("QdrantClient -")
+logger.setLevel(logging.DEBUG)
+
+
 
 # Cargar variables del .env
 load_dotenv()
@@ -23,21 +34,36 @@ load_dotenv()
 class QdrantClient:
 
     def __init__(self):
+
+        distance = {
+            'euclidean': Distance.COSINE,
+            'Manhattan': Distance.MANHATTAN,
+            'dot': Distance.DOT,
+            'cosine': Distance.COSINE
+        }
+
         self.host = os.getenv('QDRANT_HOST', 'localhost')
         self.port = int(os.getenv('QDRANT_PORT', 8100))
 
-        self.distance = os.getenv('QDRANT_DISTANCE', Distance.COSINE)
+        self.distance = distance[os.getenv('QDRANT_DISTANCE', 'cosine')]
+        self.collection_size = int(os.getenv('QDRANT_VECTOR_SIZE', 100))
         
-        qdrantClient = QdrantClient(
+        self.qdrantClient = QdrantClient(
             host= self.host, 
             port= self.port
         )
         
-    def create_collection(self, collection_name: str) -> str:
-        if not client.collection_exists(collection_name):
-            client.create_collection(
-            collection_name= collection_name,
-            vectors_config=VectorParams(size=100, distance=Distance.COSINE),
-        )
+    def create_collection(self, collection_name: str) -> bool:
+        try:
+            if not self.qdrantClient.collection_exists(collection_name):
+                self.qdrantClient.create_collection(
+                collection_name= collection_name,
+                vectors_config=VectorParams(
+                    size=self.collection_size, 
+                    distance= self.distance
+                ),
+            )
+        except Exception as e:
+
         
 
